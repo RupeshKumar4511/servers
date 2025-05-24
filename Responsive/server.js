2f2937451d3d36f170994e260c9d0b66eaf4bade
Rupesh Kumar <149661006+RupeshKumar4511@users.noreply.github.com>
2025-05-24 18:11:05 +0530

    Web server updated

--- Responsive/server.js
+++ Responsive/server.js
@@ -8,11 +8,27 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'css')));
 app.use(express.static(path.join(__dirname,'scripts')));
+app.use(express.static(path.join(__dirname,'images')));
 
+app.get('/',(req,res) =>{
+res.sendFile(path.join(__dirname,'index.html'));
+});
+
+app.get('/About.html',(req,res) =>{
+res.sendFile(path.join(__dirname,'About.html'));
+})
+
+app.get('/Service.html',(req,res)=>{
+res.sendFile(path.join(__dirname,'Service.html'));
+})
+
+app.get('/index.html',(req,res)=>{
+res.sendFile(path.join(__dirname,'index.html'));
+})
 app.post('/contact', async (req, res) => {
-    const { name, email } = req.body;
+    const { name, email,phoneNo } = req.body;
     try {
-      const [result] = await db.query('INSERT INTO users (name, email,phoneNo) VALUES (?, ?)', [name, email]);
+      const [result] = await db.query('INSERT INTO users (name, email,phoneNo) VALUES (?, ?, ?)', [name, email,phoneNo]);
       res.json({ id: result.insertId, name, email,phoneNo });
     } catch (err) {
       res.status(500).json({ error: 'Insert failed' });
